73a096afe177388be5dea0ac3e2222e3725c77e7
harsh-081004 <harshsuthar608@gmail.com>
2024-12-11 17:54:16 +0530

    more done

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
       const result = eval(Result);
       setResult(result.toString());
     } catch (error) {
-      setResult("Error");
+      setResult("bhul ave che");
     }
   };
 
@@ -32,90 +32,86 @@ const App = () => {
   const toggleSign = () => {
     setResult((prevResult) => {
       const num = parseFloat(prevResult);
-      if (isNaN(num)) {
-        return prevResult;
-      }
       return String(num * -1);
     });
   };
 
-  return ( 
-      <div className="cal">
-        <h1>Calculator</h1>
-        <form>
-          <input type="text" value={Result} readOnly />
-        </form>
-        <div className="keys">
-        <button name="Clear" onClick={clear}>
-          C
-        </button>
-        <button name="remove" onClick={remove}>
-          R
-        </button>
-        <button name="%" onClick={handleClick}>
-          %
-        </button>
-        <button name="/" onClick={handleClick}>
-          /
-        </button>
-        <br />
+  return (
+    <div className="cal">
+      <h1>calc</h1>
+      <form>
+        <input type="text" value={Result} readOnly />
+      </form>
 
-        <button name="1" onClick={handleClick}>
-          1
-        </button>
-        <button name="2" onClick={handleClick}>
-          2
-        </button>
-        <button name="3" onClick={handleClick}>
-          3
-        </button>
-        <button name="*" onClick={handleClick}>
-          x
-        </button>
-        <br />
+      <button name="Clear" onClick={clear}>
+        Clear
+      </button>
+      <button name="remove" onClick={remove}>
+        Remove
+      </button>
+      <button name="%" onClick={handleClick}>
+        %
+      </button>
+      <button name="/" onClick={handleClick}>
+        /
+      </button>
+      <br />
 
-        <button name="4" onClick={handleClick}>
-          4
-        </button>
-        <button name="5" onClick={handleClick}>
-          5
-        </button>
-        <button name="6" onClick={handleClick}>
-          6
-        </button>
-        <button name="-" onClick={handleClick}>
-          -
-        </button>
-        <br />
+      <button name="1" onClick={handleClick}>
+        1
+      </button>
+      <button name="2" onClick={handleClick}>
+        2
+      </button>
+      <button name="3" onClick={handleClick}>
+        3
+      </button>
+      <button name="*" onClick={handleClick}>
+        x
+      </button>
+      <br />
 
-        <button name="7" onClick={handleClick}>
-          7
-        </button>
-        <button name="8" onClick={handleClick}>
-          8
-        </button>
-        <button name="9" onClick={handleClick}>
-          9
-        </button>
-        <button name="+" onClick={handleClick}>
-          +
-        </button>
-        <br />
+      <button name="4" onClick={handleClick}>
+        4
+      </button>
+      <button name="5" onClick={handleClick}>
+        5
+      </button>
+      <button name="6" onClick={handleClick}>
+        6
+      </button>
+      <button name="-" onClick={handleClick}>
+        -
+      </button>
+      <br />
 
-        <button name="togglesign" onClick={toggleSign}>
-          +/-
-        </button>
-        <button name="0" onClick={handleClick}>
-          0
-        </button>
-        <button name="." onClick={handleClick}>
-          .
-        </button>
-        <button name="result" className="result" onClick={calculate}>
-          =
-        </button>
-        </div>
-        </div>
+      <button name="7" onClick={handleClick}>
+        7
+      </button>
+      <button name="8" onClick={handleClick}>
+        8
+      </button>
+      <button name="9" onClick={handleClick}>
+        9
+      </button>
+      <button name="+" onClick={handleClick}>
+        +
+      </button>
+      <br />
+
+      <button name="togglesign" onClick={toggleSign}>
+        +/-
+      </button>
+      <button name="0" onClick={handleClick}>
+        0
+      </button>
+      <button name="." onClick={handleClick}>
+        .
+      </button>
+      <button name="result" className="result" onClick={calculate}>
+        =
+      </button>
+    </div>
   );
 };
 
